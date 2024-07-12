cc7c5c327a1259036b0785e5011286ebe69853c1
JINU98 <jinendramalekar@gmail.com>
2024-07-12 17:07:56 -0400

    opt and gpt model

---
+++ scalesim/scale_decoder.py
@@ -0,0 +1,12 @@
+import os
+from scalesim.scale_sim import scalesim
+directory_path = '/home/jmalekar/Documents/scale-sim-v2/topologies/decoder_models'
+directory_contents = os.listdir(directory_path)
+config='/home/jmalekar/Documents/scale-sim-v2/configs/google.cfg'
+gemm_input = "gemm"
+
+for i in directory_contents:
+    topology='/home/jmalekar/Documents/scale-sim-v2/topologies/decoder_models/'+i
+    logpath="../test_runs/"+i
+    s = scalesim(save_disk_space=True, verbose=True,config=config,topology=topology,input_type_gemm=gemm_input)
+    s.run_scale(top_path=logpath)
