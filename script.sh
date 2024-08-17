f326b42f1d42a4b06331c05200533f45913d7a83
JINU98 <jmalekar@login001.cm.cluster>
2024-08-16 22:17:02 -0400

    clean

--- script.sh
+++
@@ -1,7 +0,0 @@
-python /work/jmalekar/scale-sim-v2/scalesim/scale_decoder.py -ts /work/jmalekar/scale-sim-v2/topologies/opt_models -c /work/jmalekar/scale-sim-v2/configs/cloud/is.cfg > opt_cloud_is.txt
-python /work/jmalekar/scale-sim-v2/scalesim/scale_decoder.py -ts /work/jmalekar/scale-sim-v2/topologies/opt_models -c /work/jmalekar/scale-sim-v2/configs/cloud/ws.cfg > opt_cloud_ws.txt
-python /work/jmalekar/scale-sim-v2/scalesim/scale_decoder.py -ts /work/jmalekar/scale-sim-v2/topologies/opt_models -c /work/jmalekar/scale-sim-v2/configs/cloud/os.cfg > opt_cloud_os.txt
-
-python /work/jmalekar/scale-sim-v2/scalesim/scale_decoder.py -ts /work/jmalekar/scale-sim-v2/topologies/opt_models -c /work/jmalekar/scale-sim-v2/configs/edge/is.cfg > opt_edge_is.txt
-python /work/jmalekar/scale-sim-v2/scalesim/scale_decoder.py -ts /work/jmalekar/scale-sim-v2/topologies/opt_models -c /work/jmalekar/scale-sim-v2/configs/edge/ws.cfg > opt_edge_ws.txt
-python /work/jmalekar/scale-sim-v2/scalesim/scale_decoder.py -ts /work/jmalekar/scale-sim-v2/topologies/opt_models -c /work/jmalekar/scale-sim-v2/configs/edge/os.cfg > opt_edge_os.txt
